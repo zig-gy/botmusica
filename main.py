b95b96fa1b1153930e6d88f6b865385e3f151612
ziggy <benjamin.f.chadwick@proton.me>
2024-06-18 22:56:14 -0400

    Creacion de bot y music_cog

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 """
     Chochetongo
 """
+
+from discord_components import ComponentsBot
+from music_cog import music_cog
+
+bot = ComponentsBot(command_prefix = '!') #Para activar bot con la !
+bot.add_cog(music_cog(bot= bot))
+
+#Abrir token.txt y leer el token del bot y conectarlo al bot
+with open('token.txt', 'r') as file:
+    token = file.readlines()[0]
+    print(token)
+bot.run(token)
---
+++ music_cog.py
@@ -0,0 +1,19 @@
+"""_summary_
+
+El cog de musica del bot, viva
+"""
+
+import discord
+from discord_components import Select, SelectOption, Button
+from discord.ext import commands
+import asyncio
+from asyncio import run_coroutine_threadsafe
+from urllib import parse, request
+import re
+import json
+import os
+from youtube_dl import YoutubeDL
+
+class music_cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
