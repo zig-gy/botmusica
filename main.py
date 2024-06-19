f91bce3c42edc11b16758a69b0d43b97ecdbb023
ziggy <benjamin.f.chadwick@proton.me>
2024-06-19 14:40:41 -0400

    Cambiado forma de guardar el token por una mas segura con python-dotenv

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 """
     Chochetongo
 """
-
+from dotenv import load_dotenv
+import os
 from discord_components import ComponentsBot
 from music_cog import music_cog
 
 bot = ComponentsBot(command_prefix = '!') #Para activar bot con la !
 bot.add_cog(music_cog(bot= bot))
 
-#Abrir token.txt y leer el token del bot y conectarlo al bot
-with open('token.txt', 'r') as file:
-    token = file.readlines()[0]
-    print(token)
-bot.run(token)
+#Abrir token y aplicarlo al bot
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+bot.run(TOKEN)
